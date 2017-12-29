8496f4e8a566d3669b1785791cc4a2d395c64312
Mark <mark@witsec.nl>
2017-12-29 15:59:10 +0100

    Updated code so you're able to turn the extension off again.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
             load: function() {
                 var a = this;
                 a.addFilter("publishHTML", function(b) {
-					var c = a.projectSettings["white-label"] || "";
+					var c = a.projectSettings["witsec-white-label"] || "";
 
 					if (c) {
 						b = b.replace(/<meta name="generator".+>/img, '')
@@ -16,13 +16,16 @@
 					return b
 				});
                 a.addFilter("additionalPageSettings", function(b) {
-					var c = a.projectSettings["white-label"] || "";
+					var c = a.projectSettings["witsec-white-label"] || "";
 					return b + [
 							'<div class="form-group clearfix">\n<label class="col-md-3 control-label">White Label</label>\n<div class="togglebutton col-md-9">\n<label style="width: 100%;">',
-							'<input type="checkbox" id="site-settings-white-label" name="white-label" ' + ("" != c ? "checked" : "") + '><span class="toggle" style="margin-top: -6px;"></span>', 
+							'<input type="checkbox" id="site-settings-witsec-white-label" name="witsec-white-label" ' + ("" != c ? "checked" : "") + '><span class="toggle" style="margin-top: -6px;"></span>', 
 							"</label>\n</div>", 
 							"</div>"
 							].join("\n")
+                });
+				a.$body.on("change", "#site-settings-witsec-white-label", function() {
+                    jQuery(this).prop("checked") ? (a.projectSettings["witsec-white-label"]="on") : (a.projectSettings["witsec-white-label"]="")
                 })
             }
         }
